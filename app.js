ffb0583fad059e5d835da688305165fe8492631d
baidu123987 <gongxi910@gmail.com>
2024-10-09 23:42:31 +0700

    app.js

--- app.js
+++ app.js
@@ -8,14 +8,7 @@ new Vue({
                         { id: 2, img: 'https://imgsrc.baidu.com/forum/pic/item/a686c9177f3e6709194fd7dc7dc79f3df8dc5542.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 2', extra: 'alert-danger' },
                         { id: 3, img: 'https://imgsrc.baidu.com/forum/pic/item/d043ad4bd11373f08377fdbce20f4bfbfbed044c.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 3' }
                     ],
-					app2: [
-                        { id: 1, img: 'https://img.911787.com/images/6698feff095eca5d32bd28df.gif', href: 'https://example.com/page1', title: ' 1' },
-                        { id: 2, img: 'https://cosmo100.top/107f3bebdf35e2795dbadf8c5f5d6a41.gif', href: 'https://example.com/page2', title: ' 2', extra: 'alert-danger' },
-                        { id: 3, img: 'https://www.n55cpw.vip/240140.gif', href: 'https://example.com/page3', title: ' 3' },
-						{ id: 4, img: 'https://ky888.tu0379199038.cc:8686/8888/xm/5088/320x185.gif', href: 'https://example.com/page3', title: ' 4' },
-						{ id: 5, img: 'https://imgoss0330.top/dbbd2052c65aed02ef70b9936fea78cb.gif', href: 'https://example.com/page3', title: ' 6' },
-						{ id: 6, img: '', href: 'https://example.com/page3', title: ' 广告位招租' }
-                    ]
+					
                 };
             }
         });
