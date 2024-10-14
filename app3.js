92847d11cd6e3cb16c86f261b05d208507b9f1f3
baidu123987 <gongxi910@gmail.com>
2024-10-14 13:56:24 +0700

    app3.js

--- app3.js
+++ app3.js
@@ -3,7 +3,7 @@ new Vue({
             data() {
                 return {
                     list: [
-                        { id: 1, img: '1111112', href: 'https://ml102.yfahx.cn/35.html', title: ' 11' },
+                        { id: 1, img: '11111123', href: 'https://ml102.yfahx.cn/35.html', title: ' 11' },
                         { id: 2, img: 'https://imgsrc.baidu.com/forum/pic/item/a686c9177f3e6709194fd7dc7dc79f3df8dc5542.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 2', extra: 'alert-danger' },
                         { id: 3, img: 'https://imgsrc.baidu.com/forum/pic/item/d043ad4bd11373f08377fdbce20f4bfbfbed044c.jpg', href: 'https://ml102.yfahx.cn/35.html', title: ' 3' }
                     ],
